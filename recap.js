3ad4eb146b69a469893654afd00c4a6a24c985c2
piasmajumdar <piasmajumdar@gmail.com>
2026-02-01 03:45:04 +0530

    first commit

---
+++ recap.js
@@ -0,0 +1,18 @@
+// const: value will not change
+// let: value can change
+// var: we should not use it
+
+
+// variable
+const amount = 200;
+
+//condition 
+if (amount > 100) {
+
+}
+else {
+
+}
+
+//array
+const numbers = [10, 20, 20, 30 ,30, 49];

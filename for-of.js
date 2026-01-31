5887733dea790a716af5941883f08c1475c23420
piasmajumdar <piasmajumdar@gmail.com>
2026-02-01 03:56:40 +0530

    for loop

---
+++ for-of.js
@@ -0,0 +1,19 @@
+const numbers = [23, 45, 56, 45, 45, 64];
+
+console.log(numbers);
+
+for(const num of numbers) {
+    console.log(num);
+}
+
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits) {
+    console.log(fruit);
+    console.log('I want to eat');
+}

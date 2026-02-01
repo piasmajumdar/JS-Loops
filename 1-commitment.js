d546fa964da2ae5924c9e37f7910913e399df924
piasmajumdar <piasmajumdar@gmail.com>
2026-02-01 15:21:08 +0530

    practice task 1 completed

---
+++ 1-commitment.js
@@ -0,0 +1,17 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+for(let i=1; i<=60; i++) {
+    console.log(i, ': I will invest at least 6 hrs every single day for next 60 days!');
+}
+
+let i = 1;
+while (i<=60) {
+    console.log(i, ': I will invest at least 6 hrs every single day for next 60 days!');
+    i++;
+}

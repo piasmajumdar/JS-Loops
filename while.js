bb6a3b3f19b7669da5b16cf058472a47ecbda2fb
piasmajumdar <piasmajumdar@gmail.com>
2026-02-01 09:56:52 +0530

    while loop

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if(number < 1) {
+    console.log('choto ekta number paisi');
+}
+console.log('something new is cooking')
+
+//---------WHILE--LOOP__________
+let num = 0;
+while(num<5) {
+    console.log('loooooping', num)
+    // num = num + 1;
+    // num += 1;
+    num++;
+}
